c7e9f008eb860171a8103b9686f3fe61a4aee572
woody35545 <woody35545@gmail.com>
2023-04-29 16:58:29 +0900

    [~] check_is_server에서 pgrep 관련 코드 수정(Mac os 환경에서는 동작 안함에 주의)

--- week7/commData_final_stu.c
+++ week7/commData_final_stu.c
@@ -542,8 +542,8 @@ void check_is_server(char *const *argv)
     }
     pclose(fp);
 	// 처음 켜지는 쪽이 server가 되도록 설정
-    if(process_count == 0) is_server = 1; else is_server = 0;
-	//is_server = process_count == 1;
+    //if(process_count == 0) is_server = 1; else is_server = 0;
+	is_server = process_count == 1;
 	//printf("[!] process_count: %d\n", process_count);
 	printf("[Debug] check_is_server: is_server: %d\n", is_server);
 
