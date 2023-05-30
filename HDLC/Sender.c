009f3bb718f2afa2336965820b893fa1b5a0ca2c
woody35545 <woody35545@gmail.com>
2023-05-30 22:49:22 +0900

    [+] Add HDLC Packet structure, testing in progress

--- HDLC/Sender.c
+++ HDLC/Sender.c
@@ -10,6 +10,26 @@
 #define IPADDR "127.0.0.1"
 #define PORT 8810
 #define PORTT 8811
+#define DEFAULT_FLAG 0x7E
+#define SABM 0x01
+#define UA 0x02
+#define DISC 0x03
+
+
+struct hdlc
+{
+    char opening_flag;
+    char addr;
+    char control;
+    char data[MAX_SIZE];
+    char closing_flag;
+};
+int isConnected = -1; // Connect 또는 Disconnect 상태를 저장하기 위한 변수. 1: connect, -1: disconnect
+
+struct hdlc;
+void set_hdlc_addr(struct hdlc * hdlc_packet, char addr);
+void debug_hdlc_packet(struct hdlc hdlc_packet);
+void send_sabm(char *input, int length);
 
 
 
@@ -22,7 +42,6 @@ struct sockaddr_in s_addr, r_addr;
 void setSocket(void);
 void* do_thread(void*);
 
-int isConnected = -1; // Connect 또는 Disconnect 상태를 저장하기 위한 변수. 1: connect, -1: disconnect
 
 
 int main(void)
@@ -40,18 +59,42 @@ int main(void)
     printf("================\n");
 
     // 사용자 메뉴 선택
+    /* [#] 유효한 값이 입력되지 않으면 오류 메시지 출력되도록 수정예정  */
     scanf("%d", &select);
     if(select > 3){printf("Invalid number!\n");}
     if(select == 1){
         printf("[DEBUG] Connect selected!\n");
         
+        /* for test */
+        send_sabm(NULL, NULL);
+
+
         // After finishing Connect
         isConnected = 1;
     }
     if(select == 2){
         if(isConnected == -1) {printf("[DEBUG] Connect first!\n");}
         else{
-        printf("[DEBUG] Chat selected!\n");}
+            printf("[DEBUG] Chat selected!\n");
+            printf("[DEBUG] Chat started!\n");
+
+            setSocket();
+            pthread_t t_id;
+            int status = pthread_create(&t_id, NULL, do_thread, NULL);
+            if (status != 0) {
+                printf("Thread Error!\n");
+                exit(1);
+            }
+            while (1) {
+                fflush(stdin);
+                fflush(stdout);
+                printf("[Send] ");
+                fgets(input, sizeof(input), stdin);
+                chat_send(input, strlen(input));   
+            }
+        }
+
+
     }
      if(select == 3){
         if(isConnected == -1) {printf("[DEBUG] Connect first!\n");}
@@ -63,20 +106,6 @@ int main(void)
         }
     
     }
-    }
-
-
-
-    setSocket();
-    pthread_t t_id;
-    int status = pthread_create(&t_id, NULL, do_thread, NULL);
-    if (status != 0) {
-        printf("Thread Error!\n");
-        exit(1);
-    }
-    while (1) {
-        fgets(input, sizeof(input), stdin);
-        chat_send(input, strlen(input));   
     }
 }
 
@@ -137,6 +166,48 @@ void setSocket()
 void chat_send(char* data, int length)
 {
     sendto(sndsock, data, length, 0, (struct sockaddr*)&s_addr, sizeof(s_addr));
+}
+
+
+// for checking values in hdlc packet
+void debug_hdlc_packet(struct hdlc hdlc_packet){
+    
+    printf("openning_flag: %#0.2x\n", hdlc_packet.opening_flag);
+    printf("addr: %c\n", hdlc_packet.addr);
+    printf("control: %#0.2x\n", hdlc_packet.control);
+    printf("closing_flag: %#0.2x\n", hdlc_packet.closing_flag);
+    printf("\n");
+}
+
+
+void set_hdlc_addr(struct hdlc * hdlc_packet, char addr){ 
+    hdlc_packet->addr = addr;
+}
+void set_hdlc_control(struct hdlc * hdlc_packet, char control){ 
+    hdlc_packet->control = control;
+}
+void set_hdlc_flag(struct hdlc * hdlc_packet, char flag){ 
+    hdlc_packet->opening_flag = flag;
+}
+void set_hdlc_cflag(struct hdlc * hdlc_packet, char cflag){ 
+    hdlc_packet->closing_flag = cflag;
+}
+
+
+/* Function for testing HDLC packet*/
+void send_sabm(char *input, int length){
+    struct hdlc hdlc_packet;
+
+    set_hdlc_flag(&hdlc_packet, DEFAULT_FLAG);
+    set_hdlc_addr(&hdlc_packet, 'B');
+    set_hdlc_control(&hdlc_packet, SABM); 
+    set_hdlc_cflag(&hdlc_packet, DEFAULT_FLAG);
+
+    debug_hdlc_packet(hdlc_packet);
+
+    // send hdlc packet
+    //printf("[DEBUG] $ sendto(sndsock, &hdlc_packet, sizeof(hdlc_packet), 0, (struct sockaddr*)&s_addr, sizeof(s_addr));");
+    sendto(sndsock, &hdlc_packet, sizeof(hdlc_packet), 0, (struct sockaddr*)&s_addr, sizeof(s_addr));
 }
 
 
